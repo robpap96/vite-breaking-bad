dad647313ae23474ae0a56cee766cad1c92c8069
robpap96 <robertopapagni1996@outlook.it>
2022-11-30 00:57:37 +0100

    fix card style

--- vite-template/src/components/CharacterCard.vue
+++ vite-template/src/components/CharacterCard.vue
@@ -30,9 +30,9 @@ export default {
     <div class="card mb-5" v-for="character in store.Characters">
         <img :src="`${character.img}`" class="card-img-top" :alt="`${character.name}`">
         <div class="card-body">
-            <h5 class="card-title">{{character.name}}</h5>
+            <h3 class="card-title">{{character.name}}</h3>
             <h4 class="card-title">{{character.category}}</h4>
-            <h3 class="card-title">{{character.status}}</h3>
+            <h5 class="card-title">{{character.status}}</h5>
         </div>
 
 
@@ -43,9 +43,27 @@ export default {
 <style lang="scss" scoped>
     .card{
         width: calc(100% / 5 - 30px);
+        background-color: #2e3a46;
+        border-radius: 0px;
         img{
             width: 100%;
             height: 250px;
+            margin-top: 10px;
+            border-radius: 0px;
+        }
+    }
+    .card-body{
+        h3{
+            font-size: 20px;
+            color: rgb(225, 234, 242);
+        }
+        h4{
+            font-size: 18px;
+            color: rgb(107, 107, 107);
+        }
+        h5{
+            font-size: 17px;
+            color: rgb(107, 107, 107);
         }
     }
 </style>
